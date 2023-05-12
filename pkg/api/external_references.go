58b73ee0b0095b7be1596ecd06078aeb9eb1c525
toshski <toshski8760@gmail.com>
2023-05-12 14:25:51 +1200

    External Reference Table

--- pkg/api/external_references.go
+++ pkg/api/external_references.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	restfulspec "github.com/emicklei/go-restful-openapi/v2"
 	"github.com/emicklei/go-restful/v3"
 )
 
@@ -17,5 +18,10 @@ func (i ExternalReference) WebService() *restful.WebService {
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 
+	ws.Route(ws.GET("/generic/scrape_all").To(i.genericActorScraper).
+		Metadata(restfulspec.KeyOpenAPITags, tags))
+
+	ws.Route(ws.POST("/generic/scrape_single").To(i.genericSingleActorScraper).
+		Metadata(restfulspec.KeyOpenAPITags, tags))
 	return ws
 }
